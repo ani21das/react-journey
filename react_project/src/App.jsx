3bf4ce6e8b5a3c3afee32a3a20c249ca0fe2f56f
Anirban <anirban21das@gmail.com>
2023-09-21 12:26:50 +0530

    counter app

--- react_project/src/App.jsx
+++ react_project/src/App.jsx
@@ -1,12 +1,37 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
-function App() {
+function App () {
+
+  let [ counter, setCounter ] = useState( 0 );
+
+  const addValue = () => {
+    if ( counter < 20 )
+    {
+      counter = ++counter;
+    }
+    setCounter( counter );
+    console.log( "Clicked", counter );
+  };
+
+  const removeValue = () => {
+    if ( counter > 0 )
+    {
+      counter = --counter;
+    }
+    setCounter( counter );
+    console.log( "Clicked;", counter );
+  };
 
   return (
     <>
-        <h1>Hii</h1>
+      <h1>Counter value : { counter }</h1>
+      <br />
+      <button onClick={ addValue }>Add Value</button>
+      <br />
+      <button onClick={ removeValue }>Remove Value</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
